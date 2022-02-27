59702ba3f662f069b05f6cfb1099462ba803de6c
nafis79 <100079056+nafis79@users.noreply.github.com>
2022-02-28 01:44:07 +0330

    Add files via upload

---
+++ Exercise6-P63 .py
@@ -0,0 +1,9 @@
+#Exercise 6 Page 63 PythonBook
+x = 1
+print("x")
+print('x')
+print(x) #+
+print("x + 1")
+# print('x' + 1) This command is incorrect and causes an error 
+# because the string data cannot be summed with int data 
+print(x + 1) #+
