863d1886f9922dde5847a957dd4197085645f5da
nafis79 <100079056+nafis79@users.noreply.github.com>
2022-03-14 02:45:29 +0330

    Update Exe1(w4).py

--- Exe1(w4).py
+++ Exe1(w4).py
@@ -1,16 +1,11 @@
-max_value = int(input('Display primes up to what value? '))
-value = 2 
-while value <= max_value:
-  is_prime = True 
-  trial_factor = 2
-  while trial_factor < value:
-       if value % trial_factor == 0:
-          is_prime = False 
-          break
-       trial_factor += 1
-  if is_prime:
-       print(value, end='') 
-  value += 1
-print() 
-
-#Display prices up to what value? 90 2357317 19 23 29-3174113473 59 61 67713 79 83 89
+MAX = 20
+n=1
+while n >= MAX:
+  factor = 1
+  print(end=str(n) + ': ')
+  while factor <= n:
+    if n % factor == 0:
+      print(factor, end=' ') 
+      factor += 1
+  print() 
+  n += 1
