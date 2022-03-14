a00551faa4882009e8c6d3f258d98f63183b661b
nafis79 <100079056+nafis79@users.noreply.github.com>
2022-03-14 13:25:27 +0330

    Update Exe1(w4).py

--- Exe1(w4).py
+++ Exe1(w4).py
@@ -1,11 +1,11 @@
-MAX = 20
+MAX = 5
 n=1
-while n >= MAX:
+while n <= MAX:
   factor = 1
   print(end=str(n) + ': ')
   while factor <= n:
     if n % factor == 0:
       print(factor, end=' ') 
-      factor += 1
+    factor += 1
   print() 
   n += 1
